04a7d4c50bea9217b7ad6a3df150b9da88248975
Numan <numaniftikhar300@gmail.com>
2022-09-05 16:50:22 +0500

    add icons file

---
+++ assets/icons.js
@@ -0,0 +1,12 @@
+const icons = [
+  `<i class="fa-solid fa-house"></i>`,
+  `<i class="fa-solid fa-address-card"></i>`,
+  `<i class="fa-solid fa-message"></i>`,
+  `<i class="fa-solid fa-clock"></i>`,
+  `<i class="fa-solid fa-files"></i>`,
+  `<i class="fa-solid fa-user-group"></i>`,
+  `<i class="fa-solid fa-gear"></i>`,
+  `<i class="fa-solid fa-square-question"></i>`,
+  `<i class="fa-solid fa-shield-check"></i>`,
+  `<i class="fa-sharp fa-solid fa-objects-column"></i>`,
+];
